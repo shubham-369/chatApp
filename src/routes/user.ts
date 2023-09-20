ea8f57ef67ffe374164827361f1131634c52cd8d
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-09-20 18:56:53 +0530

    Delete src directory

--- src/routes/user.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-const router = Router();
-import * as userControllers from "../controllers/user";
-
-router.post('/signup', userControllers.signup);
-
-export default router;
--- src/util/database.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const sequelize = new Sequelize('chatapp', 'root', 'r333@666m999', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-
-export default sequelize;
